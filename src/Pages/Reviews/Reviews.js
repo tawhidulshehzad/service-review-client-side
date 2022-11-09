70a4db764ef0d0945fd00380a4c06a402cb4826a
shehzad1212 <tawhidulislam261@gmail.com>
2022-11-09 20:56:32 +0600

    table adding

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,11 +1,18 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const Reviews = () => {
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
+  const [reviews, setReviews] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/reviews?email=${user?.email}`)
+      .then((response) => response.json())
+      .then((data) => setReviews(data));
+  }, [user?.email]);
   return (
     <div>
-      
+      <h2>You have {reviews.length} reviews</h2>
     </div>
   );
 };
